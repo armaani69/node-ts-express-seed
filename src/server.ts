b17ea2ec7f1ed00bc20e7c3e635014a4ba065491
Armaan Mathew <armaan.mathew@neoito.com>
2022-04-06 14:15:51 +0530

    first commit

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+require('dotenv-safe').config({
+	allowEmptyValues: true,
+});
+import App from '~/app';
+import AuthRoute from '~/routes/auth.route';
+import IndexRoute from '~/routes/index.route';
+import UserRoute from '~/routes/user.route';
+import validateEnv from '~/utils/validateEnv';
+
+validateEnv();
+
+const app = new App([new IndexRoute(), new AuthRoute(), new UserRoute()]);
+
+app.listen();
